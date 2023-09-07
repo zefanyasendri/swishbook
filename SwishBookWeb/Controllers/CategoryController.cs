c016f27dd265fc9c4e1ffee04dced3676b26e746
zefanyasendri <zefanyasendri@gmail.com>
2023-09-07 13:06:41 +0700

    feat: Add Create and Update

--- SwishBookWeb/Controllers/CategoryController.cs
+++ SwishBookWeb/Controllers/CategoryController.cs
@@ -19,5 +19,67 @@ namespace SwishBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //not required
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder can't exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                // return redirect to index
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Update(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryDb = _db.Categories.Find(id);
+            // var categoryDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // var categoryDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //not required
+        public IActionResult Update(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder can't exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                // return redirect to index
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
